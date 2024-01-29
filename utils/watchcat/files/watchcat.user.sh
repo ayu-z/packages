5a1e4aee40e63887dd932b94793c85e4e84082fb
yuzui66 <yuzuihong@live.com>
2024-01-29 17:51:47 +0800

    update n2n

--- utils/watchcat/files/watchcat.user.sh
+++ utils/watchcat/files/watchcat.user.sh
@@ -6,31 +6,31 @@ logger 'run watchcat user sh'
 ATtool=ylx_AT-Tool
 switch=$(uci get watchcat.@watchcat[0].simslot)
 vidpid=`cat /sys/kernel/debug/usb/devices |grep Vendor |grep -v "Vendor=1d6b"|head -n 1 |awk  '{print $2  $3}'`
-atDev=/dev/$(cat /tmp/modem.json |grep atDev |awk -F':' '{print $2}' |tr -d '"", ')
+atDev=$(cat /tmp/modem.json |grep atDev |awk -F':' '{print $2}' |tr -d '"", ')
 simslot=$(cat /tmp/modem.json |grep SimSlot |awk -F':' '{print $2}' |tr -d '"", ')
 [ -z "$simslot"] && simslot='1'
-[ "$atDev"x == "/dev/"x ] && echo "Fetch device error" && exit 
+[ -z "$atDev" ] && logger "Fetch device error" && exit 
 
 sendCmd(){
     cmd=$1
     $ATtool -p "$atDev" -c "$cmd"
 }
 
-if [ "$switch" == "1" ]; then
+if [ "$switch"x == "1"x ]; then
     case $vidpid in
         Vendor=2deeProdID=4d52)
             if [ "$simslot" == "1" ]; then
                 sendCmd "AT^SIMSLOT=2"
                 sleep 3
-                reg_net=$(sendCmd "at+cpin?"|grep CPIN|cut -d" " -f2|tr -d "\r\n")
-                if [ "$reg_net" != "READY" ]; then
+                simstate=$(sendCmd "at+cpin?"|grep CPIN|cut -d" " -f2|tr -d "\r\n")
+                if [ "$simstate" != "READY" ]; then
                     sendCmd "AT^SIMSLOT=1"
                     sendCmd "AT+CFUN=1,1"
                 fi
             else
                 sendCmd "AT^SIMSLOT=1"
-                reg_net=$(sendCmd "at+cpin?"|grep CPIN|cut -d" " -f2|tr -d "\r\n")
-                if [ "$reg_net" != "READY" ]; then
+                simstate=$(sendCmd "at+cpin?"|grep CPIN|cut -d" " -f2|tr -d "\r\n")
+                if [ "$simstate" != "READY" ]; then
                     sendCmd "AT^SIMSLOT=2"
                     sendCmd "AT+CFUN=1,1"
                 fi
@@ -40,16 +40,16 @@ if [ "$switch" == "1" ]; then
             if [ "$simslot" == "1" ]; then
                 sendCmd "AT+QUIMSLOT=2"
                 sleep 3
-                reg_net=$(sendCmd "at+cpin?"|grep CPIN|cut -d" " -f2|tr -d "\r\n")
-                if [ "$reg_net" != "READY" ]; then
+                simstate=$(sendCmd "at+cpin?"|grep CPIN|cut -d" " -f2|tr -d "\r\n")
+                if [ "$simstate" != "READY" ]; then
                     sendCmd "AT+QUIMSLOT=1"
                     sendCmd "AT+CFUN=1,1"
                 fi
             else
                 sendCmd "AT+QUIMSLOT=1"
                 sleep 3
-                reg_net=$(sendCmd "at+cpin?"|grep CPIN|cut -d" " -f2|tr -d "\r\n")
-                if [ "$reg_net" != "READY" ]; then
+                simstate=$(sendCmd "at+cpin?"|grep CPIN|cut -d" " -f2|tr -d "\r\n")
+                if [ "$simstate" != "READY" ]; then
                     sendCmd "AT+QUIMSLOT=2"
                     sendCmd "AT+CFUN=1,1"
                 fi
